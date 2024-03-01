3dbc4ca8fa4ee6ce184ffe63da6a2a5d82cc65fb
Tony Eder <tonyeder11@gmail.com>
2024-03-01 09:18:50 -0800

    9:20am - all properties instantiated. working to activate relationship between classes via joiner class Orders

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,6 +1,12 @@
 class Coffee:
     def __init__(self, name):
         self._name = name
+
+        # attribute for Customers
+        self._customers = []
+
+        # attribute for Order
+        self._order = []
     
     @property
     def name(self):
@@ -11,6 +17,9 @@ class Coffee:
         if not hasattr(self, 'name'):
             if isinstance(name, str) and len(name) > 3:
                 self._name = name
+
+    def orders(self):
+        pass
     
     def customers(self):
         pass
@@ -21,18 +30,27 @@ class Coffee:
     def average_price(self):
         pass
 
-class Customer:
-    def __init__(self, cust_name):
-        self.cust_name = cust_name
+    def __repr__(self):
+        return f"Coffee: {self.name}"
 
-    # @property
-    # def name(self):
-    #     return self._name
+class Customer:
+    def __init__(self, name):
+        self.name = name
+
+        # attritute for Coffee
+        self._coffees = []
+
+        # attribute for Orders
+        self._orders = []
+
+    @property
+    def name(self):
+        return self._name
     
-    # @name.setter
-    # def name(self, name):
-    #     if isinstance (name, str) and 1 < len(name) < 15:
-    #         self._name = name
+    @name.setter
+    def name(self, name):
+        if isinstance (name, str) and 1 < len(name) < 15:
+            self._name = name
         
     def orders(self):
         pass
@@ -43,8 +61,30 @@ class Customer:
     def create_order(self, coffee, price):
         pass
     
+    def __repr__(self):
+        return f"Customer: {self.name}"
+
+    
 class Order:
+    
+    all = []
+
     def __init__(self, customer, coffee, price):
         self.customer = customer
         self.coffee = coffee
         self.price = price
+        Order.all.append(self)
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, price):
+        if not hasattr(self, "price"):
+            if isinstance(price, float) and 1.0 < price < 10.0:
+                self._price = price
+
+
+    def __repr__(self):
+        return f"Coffee: {self.coffee}\nCustomer: {self.customer}\nPrice: {self.price}"
